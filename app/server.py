e13bca3d460686ced2a34d4844c46bffb03b5c75
Noah O <c9shroud@gmail.com>
2019-02-19 21:01:32 +0000

    changes default

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1W8n_DAbJ7G-jDOLhenMYTu9b33ANnrXV'
+export_file_name = 'web.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['sneakers',  'boots']
 path = Path(__file__).parent
 
 app = Starlette()
